bb9abbd3640fb1c092cab4a91c7df8d78ce00abf
nicomalvicini07 <nirockmalvicini@gmail.com>
2026-04-18 21:48:57 -0300

    actualización mate

---
+++ scripts/mate.gd
@@ -0,0 +1,24 @@
+extends Button
+
+@onready var animation_player: AnimationPlayer = $"../AnimationPlayer"
+
+
+var active = true
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pressed() -> void:
+	if active == true:
+		active = false
+		animation_player.play("TOMAR MATE")
+		await  get_tree().create_timer(4).timeout
+		active = true
+		
+	pass # Replace with function body.
