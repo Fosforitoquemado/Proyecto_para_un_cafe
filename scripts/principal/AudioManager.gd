207dfe0401bed100370d387b24829d5458043468
BGYT25-hub <br.social25@gmail.com>
2026-06-17 20:03:52 -0300

    Cambios Clicks
    Agregue distintos clicks, quedo el que estaba, mas corto y hay algunos como bocina o auto arrancando que estan buenos pero los tendrian que escuchar como para ver!!

--- scripts/principal/AudioManager.gd
+++ scripts/principal/AudioManager.gd
@@ -2,7 +2,7 @@ extends Node
 
 @onready var player = $AudioStreamPlayer
 
-var click_sound = preload("res://sonidos/NUEVOS SONIDOS/CLICK.mp3")
+var click_sound = preload("res://sonidos/NUEVOS SONIDOS/Clicks/CLICK MOUSE CASERO.mp3")
 
 func play_click():
 	player.stream = click_sound
